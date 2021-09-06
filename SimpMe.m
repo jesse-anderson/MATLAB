2b2013b26801dfbff7d6abcf0d0a8813a18f8e48
jesse-anderson <75538757+jesse-anderson@users.noreply.github.com>
2021-09-05 21:07:27 -0500

    Update and rename EquationSolver.m to SimpMe.m

--- SimpMe.m
+++ SimpMe.m
@@ -1,23 +1,30 @@
+%declare your variables as shown
+%syms x y t
+%equation = x*y==5*t+6;
+%SolveFor = x;
+%[Solved] = SimpMe(equation,SolveFor)
+%function [solution] = SimpMe(equation,solveFor)
 %%%Jesse Anderson
 %%Symbolic Solver for when Symbolab/Wolfram fail me to check my work
 %2020
-clear all
-close all
-clc
-syms a b c d e f g h i j k l m n o p q r s t u v w x y z
+%clear all
+%close all
+%clc
+%syms a b c d e f g h i j k l m n o p q r s t u v w x y z
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%equation = y*a*b*e^a == v*t + (1/2)*a*(exp(t)) ;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-equation = 0 == 5*t+25;%%%CHANGE ME EQUATION
-solveFor = t; %%%CHANGE ME SOLVE FOR VARIABLE
+%equation = 0 == 5*t+25;%%%CHANGE ME EQUATION
+%solveFor = t; %%%CHANGE ME SOLVE FOR VARIABLE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %solveFor = t;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-disp(equation)
+%disp(equation)
 solveFor = arrayfun(@char, solveFor, 'uniform', 0);
 solveMe = solve(equation, solveFor);
 solveMe = arrayfun(@char, solveMe, 'uniform', 0);%converts syms to strings
-solveUlt = strcat(solveFor,{' = '},solveMe);
-disp(solveUlt)
+solution = strcat(solveFor,{' = '},solveMe);
+%disp(solution)
+end
